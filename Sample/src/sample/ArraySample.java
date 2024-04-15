91f64c56f8ac6c509e081cd1c66c622c91520fb9
dir-FY24iwasakikazuma <kazuma.iwasaki.y978@dirbato.co.jp>
2024-04-15 16:26:34 +0900

    20240415

--- Sample/src/sample/ArraySample.java
+++ Sample/src/sample/ArraySample.java
@@ -9,13 +9,10 @@ public class ArraySample {
         
         for (var i = 0; i < numbers.length; i++) {
             numbers [i] = i * 2; //配列の各要素に値を代入
-        }
-        
-        //各列の要素を表示
-        for (var i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
-        
     }
+}//各列の要素を表示
+       
+   
 
-}
